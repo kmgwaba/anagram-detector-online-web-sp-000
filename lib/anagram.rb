ba427b02de37b068f72927a9c99ba9ac6c9a0ae3
Kathia Mgwaba <mgwaba68@gmail.com>
2020-07-23 13:56:12 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,5 +1,15 @@
 # Your code goes here!
 class Anagram
   attr_accessor :anagrams
-  @angrams = anagrams
+  
+  def intialize(anagrams)
+  @anagrams = anagrams
 end 
+
+def match(anagrams_array)
+  anagrams_array.select do |anagrams|
+    anagrams.split("").sort == @anagrams.split("").sort
+  end
+end
+end
+
