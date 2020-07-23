afd7fda66e202d80de6591901940d60c4649c2c2
Kathia Mgwaba <mgwaba68@gmail.com>
2020-07-23 13:57:19 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,11 +4,11 @@ class Anagram
 
   def intialize(anagrams)
   @anagrams = anagrams
-end
+  end
 
-def match(anagrams_array)
+  def match(anagrams_array)
   anagrams_array.select do |anagrams|
     anagrams.split("").sort == @anagrams.split("").sort
+    end
   end
-end
 end
